60b6cacbe3fbc12393832644bb5ab3e6faddf4d5
chuong2512 <hieutg244@gmail.com>
2023-02-14 20:29:20 +0700

    initial commit

---
+++ Assets/CAR PARKING/Script/ParkingTarget.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ParkingColor { Red, Yellow, Blue, Green}
+public class ParkingTarget : MonoBehaviour
+{
+    public ParkingColor parkingColor;
+}
