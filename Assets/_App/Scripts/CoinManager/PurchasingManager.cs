6acbba72ea9ecc581e9a9756cb037b02340438c7
chuong2512 <hieutg244@gmail.com>
2023-02-21 00:27:05 +0700

    update

---
+++ Assets/_App/Scripts/CoinManager/PurchasingManager.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurchasingManager : MonoBehaviour
+{
+    public void OnPressDown(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(1); };
+                IAPManager.Instance.BuyProductID(IAPKey.PACK1);
+                break;
+            case 2:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(2); };
+                IAPManager.Instance.BuyProductID(IAPKey.PACK2);
+                break;
+            case 3:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(5); };
+                IAPManager.Instance.BuyProductID(IAPKey.PACK3);
+                break;
+            case 4:
+                IAPManager.OnPurchaseSuccess = () => { GameDataManager.Instance.playerData.AddDiamond(10); };
+                IAPManager.Instance.BuyProductID(IAPKey.PACK4);
+                break;
+        }
+    }
+
+    public void Sub(int i)
+    {
+        GameDataManager.Instance.playerData.SubDiamond(i);
+    }
+}
